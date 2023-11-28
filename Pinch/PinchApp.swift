a999f48d668567334c15f1ff3ed49bfa7879f1d5
Jonathan Ricky <jonathan.ricky@noble-software.com>
2023-11-28 16:12:50 +0700

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Jonathan Ricky on 28/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
